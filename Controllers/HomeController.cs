608e2d7412252afe915dce4e15aaee76b7f2944d
arekak <9092672+arekak@users.noreply.github.com>
2022-12-28 22:18:37 +0100

    core2022-7A (do str. 165)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using info_2022.Models;
+﻿using info_2022.Data;
+using info_2022.Models;
+using info_2022.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace info_2022.Controllers
@@ -7,15 +10,24 @@ namespace info_2022.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeDataVM homeData = new();
+            homeData.DisplayCategories = _context.Categories?
+                .Where(c => c.Active == true && c.Display == true);
+            homeData.Authors = _context.Texts
+                .Include(a => a.User)
+                .Select(a => a.User)
+                .Distinct();
+            return View(homeData);
         }
 
         public IActionResult Privacy()
